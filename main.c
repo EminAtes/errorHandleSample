0a58a1557c5961debdc3e5c8dbd00b567a200946
Emin Ates <ennin_ates@hotmail.com>
2020-11-12 23:26:52 +0300

    added exit_sys to reduce line count

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+
+/**
+ * @brief The Developer should handle every function return value(Ofcourse there are some exceptions).
+ * So to decrease line count exit_sys wrapper function can be used. 
+ * 
+ * @param msg The message will be written output stream with dedicated error expression.
+ * 
+*/
+void exit_sys(const char * msg);
+
 int main(void)
 {
     char * filePath = "myfile.txt";
@@ -10,22 +20,14 @@ int main(void)
      * If the open function ends with success, returns the file descriptor otherwise -1.
     */
     if((open(filePath, O_RDONLY)) == -1)
-    {
-        /**
-        *   If the file cannot be opened. The "open" function will pass error number to global "errno" variable.
-        * For unix/linux system there isn't a standartization for system error number. So "switch case" statment 
-        * with "errno" expression will not be portable. Because the exact same error number may refer 
-        * different error expression for various unix/linux systems. Assume that Linux system A have "Access Denied"
-        * error with error number "9" and other linux system B may have "Access Denied" error with error number "11".
-        * Practically, Using the posix "strerror" function will return the error expression that is dedicated to error no.
-        * In this situation using "strerror" is usefull.
-        */
-        fprintf(stderr,"open: %s\n", strerror(errno));
-        /**
-         * Terminate program with failure.
-        */
-        exit(EXIT_FAILURE);
-    }
+        exit_sys("open");
+    
     printf("%s file opened.\n",filePath);
     return EXIT_SUCCESS;
+}
+
+void exit_sys(const char * msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
